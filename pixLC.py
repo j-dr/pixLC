ba3490354a774a981b54f0866d772e011c6d1c24
j-dr <jderose@stanford.edu>
2016-01-23 17:26:31 -0800

    Changed file nside to be nest ordering as well. Added more documentation

--- pixLC.py
+++ pixLC.py
@@ -126,7 +126,18 @@ def readGadgetSnapshot(filename, read_pos=False, read_vel=False, read_id=False,\
 
         
 class Buffer(object):
+    """
+    Buffer object for use when recombining cells written in map step.
+    Handles one type of data (pos, vel, ids) at a time.
+    """
     def __init__(self,fname,dtype,nmax=10000000):
+        """
+        Initialize buffer
+        
+        fname -- The filename to write to
+        dtype -- The type of data to write
+        nmax -- The max number of particles to hold at once before dumping
+        """
         self.buff = np.zeros(nmax,dtype=dtype)
         self.nmax = nmax
         self.ncurr = 0
@@ -138,6 +149,7 @@ class Buffer(object):
         self.write()
         
     def write(self):
+        #if buffer not empty, write 
         if self.ncurr > 0:
             with open(self.fname, 'ab') as fp:
                 fp.write(self.buff[0:self.ncurr].tobytes())
@@ -151,6 +163,8 @@ class Buffer(object):
         
         loc = 0
         nnew = len(d)
+        #if more particles added than can be 
+        #held in buffer, dump
         while nnew + self.ncurr > self.nmax:
             npos = self.nmax - self.ncurr
             if npos > nnew:
@@ -165,6 +179,7 @@ class Buffer(object):
             
             self.write()
             
+        #if all particles not yet in buffer, put the rest in
         if loc < len(d):
             nleft = len(d) - loc
             self.buff[self.ncurr:self.ncurr+nleft] = d[loc:loc+nleft]
@@ -172,7 +187,19 @@ class Buffer(object):
 
 
 class RBuffer(object):
+    """
+    Buffer object to use for map step. Holds position, velocity, and ids at once
+    """
+
     def __init__(self,fname,header,nmax=8000000):
+        """
+        Initialize buffer
+        
+        fname -- File name to write to
+        header -- File header to write
+        nmax -- Max number of particles to hold at once
+        """
+
         self.pbuff = np.zeros(nmax*3,dtype='f4')
         self.vbuff = np.zeros(nmax*3,dtype='f4')
         self.ibuff = np.zeros(nmax,dtype='u8')
@@ -186,12 +213,18 @@ class RBuffer(object):
         self.filenside = header[1]
         self.fileorder = int(np.log2(self.filenside))
         self.header = header
-        self.hdrfmt = 'QIIfdQddd'
+        self.hdrfmt = 'QIIfdQddd' 
 
     def sort_by_peano(self):
+        """
+        Sort particles by the peano index of the healpix cell
+        they are in
+        """
+        #determine healpix cells
         pix = hp.vec2pix(self.filenside, self.pbuff[:3*self.ncurr:3], \
                          self.pbuff[1:3*self.ncurr:3], self.pbuff[2:3*self.ncurr:3], nest=True)
         peano = nest2peano(pix, self.fileorder)
+        #sort by peano index
         pidx = np.argsort(peano)
         pix = pix[pidx]
         for i in range(3):
@@ -200,6 +233,7 @@ class RBuffer(object):
             
         self.ibuff[:self.ncurr] = self.ibuff[pidx]
 
+        #determine number of particles in each cell
         pidx = pix[1:]-pix[:-1]
         pidx = np.where(pidx!=0)[0]+1
         nidx = [0]
@@ -211,9 +245,9 @@ class RBuffer(object):
 
     def write(self):
         self.sort_by_peano()
-
+        
+        #if buffer not empty, write data to file
         if self.ncurr > 0:
-            #tprint("    writing to file '%s'" % self.fname+'.{0}'.format(self.dumpcount))
             with open(self.fname+'.{0}'.format(self.dumpcount), 'w+b') as fp:
                 fp.write(struct.pack(self.hdrfmt, *self.header))
                 fp.write(self.pidx.tobytes())
@@ -234,6 +268,9 @@ class RBuffer(object):
         loc = 0
         nnew = len(ids)
         assert nnew*3==len(pos)
+        
+        #if number of particles added greater than
+        #buffer capacity, dump
         while nnew + self.ncurr > self.nmax:
             nadd = self.nmax - self.ncurr
             
@@ -247,6 +284,7 @@ class RBuffer(object):
             
             self.write()
             
+        #if haven't stored all particles added, do so
         if loc < len(ids):
             nleft = len(ids) - loc
             self.pbuff[3*self.ncurr:3*(self.ncurr+nleft)] = pos[3*loc:3*(loc+nleft)]
@@ -255,6 +293,16 @@ class RBuffer(object):
             self.ncurr += nleft
 
 def create_refinement_plan(rmin, rmax, rstep, rr0, lfilenside, hfilenside=None):
+    """
+    Determine the nside to write files to for each radial bin
+    
+    rmin -- Minimum radius
+    rmax -- Maximum radius
+    rstep -- Radial bin size
+    rr0 -- The first radius to refine at
+    lfilenside -- Nside to use at rmin
+    hfilenside -- Max nside to use
+    """
 
     rbins = np.linspace(rmin,rmax,(rmax-rmin)//rstep+1)
 
@@ -295,20 +343,23 @@ def write_to_cells_buff(filepaths, outbase, indexnside=16, lfilenside=1,
         The radius at which we want to perform our first refinement. Further refinements
         will be performed at r_n = rr0*sqrt(2)^n.
     """
+    #determine the radii to refine the nside value of the healpix cells that
+    #the files are broken into
     rbins, rr, rnside, nr = create_refinement_plan(rmin, rmax, rstep, rr0,
                                                    lfilenside, hfilenside=hfilenside)
     rbins2 = rbins*rbins
-
 
     print('Max number of refinements: {0}'.format(nr))
     print('Maximum nside: {0}'.format(rnside[-1]))
     print('Radii to refine at: {0}'.format(rr))
     
-    header = [0, indexnside, rnside[0], boxsize, pmass, 0, 0.0, 0.0, 0.0]
+    header = [0, indexnside, rnside[0], 0.0, 0.0, 0, 0.0, 0.0, 0.0]
     ntot = 0
         
     nfiles = len(filepaths)
     buffs = {}
+    
+    #iterate over files, writing them to radial/healpix cells
     for fnum,filepath in enumerate(filepaths):
         tprint('    file %6d of %6d' % (fnum+1,nfiles))
         block = filepath.split('/')[-1].split('.')[-1]
@@ -321,6 +372,8 @@ def write_to_cells_buff(filepaths, outbase, indexnside=16, lfilenside=1,
         header[-2] = hdr.OmegaLambda
         header[-3] = hdr.Omega0
         header[-4] = hdr.npartTotal[1]
+        header[-5] = hdr.mass[1]
+        header[-6] = hdr.BoxSize
         
         r2 = pos[:,0]**2 + pos[:,1]**2 + pos[:,2]**2
 
@@ -334,7 +387,7 @@ def write_to_cells_buff(filepaths, outbase, indexnside=16, lfilenside=1,
         vel = vel[ridx,:]
         ids = ids[ridx]
         
-        #determine the healpix cell that each particle falls into
+        #determine the healpix cell of parts using largest radial bin's nside 
         pix = hp.vec2pix(rnside[-1], pos[:,0], pos[:,1], pos[:,2], nest=True)
         idxdtype = np.dtype([('pidx', np.int32), ('ridx', np.int32)])
         bins = np.ndarray(len(pos), dtype=idxdtype)
@@ -368,13 +421,17 @@ def write_to_cells_buff(filepaths, outbase, indexnside=16, lfilenside=1,
                 end = len(bins)
             else:
                 end = idx[i+1]
-
+            
             rind = bins['ridx'][start]
             header[2] = rnside[rind]
-            print('Working on radial bin with nside = {0}'.format(rnside[rind]))
-            pix = hp.vec2pix(rnside[rind], pos[start:end,0], pos[start:end,1],
-                             pos[start:end,2])
 
+            print('Working on radial bin with nside = {0}'.format(rnside[rind]))
+            #Determine hpix cell of parts with nside for this radial bin
+            pix = hp.vec2pix(rnside[rind], pos[start:end,0], pos[start:end,1],
+                             pos[start:end,2], nest=True)
+            
+            #Create index into hpix values, already sorted since sorted by
+            #largest radial bin's nside earlier
             pinc = pix[1:]-pix[:-1]
             pidx, = np.where(pinc!=0)
             pidx = list(pidx+1)
@@ -382,6 +439,7 @@ def write_to_cells_buff(filepaths, outbase, indexnside=16, lfilenside=1,
             nidx.extend(pidx)
             pidx = np.array(nidx,dtype='i8')
             
+            #iterate over hpix and write parts to cells
             for j, pstart in enumerate(pidx):
                 if j==(len(pidx)-1):
                     pend = len(pix)
@@ -390,37 +448,41 @@ def write_to_cells_buff(filepaths, outbase, indexnside=16, lfilenside=1,
                     
                 pind = pix[pstart]
                 deltan = pend - pstart
-                #tprint('    Number of particles in bin {0},{1}: {2}'.format(rind, pind, deltan))
+
                 nwrit += deltan
                 if rind not in buffs:
                     buffs[rind] = {}
                 if pind not in buffs[rind]:
                     buffs[rind][pind] = RBuffer(outbase+'_{0}_{1}_{2}'.format(rind, pind, block),
                                                 header, nmax=buffersize)
-
+                    
                 buffs[rind][pind].add(pos[start+pstart:start+pend,:].flatten(),
                                       vel[start+pstart:start+pend,:].flatten(),
                                       ids[start+pstart:start+pend])
-            
+        
         assert nwrit == len(bins)
-        #tprint('    put data in buff')
         ntot += len(bins)
         
-    #tprint('    writing outputs')
     nwrit = 0
+    #write out particles left in buffers, make sure we wrote everything
     for rind in buffs.keys():
         for pind in buffs[rind].keys():
             buffs[rind][pind].write()
             nwrit += buffs[rind][pind].nwritten
-            #tprint('    Buffer for bin {0}, {1} dumped {2} times'.format(rind,pind,buffs[rind][pind].dumpcount))
             del buffs[rind][pind]
 
-    #tprint('    Total number of particles written, read: {0}, {1}'.format(nwrit, ntot))
+    assert( nwrit == ntot )
+
     return header
 
 
 def combine_cell_list(flist):
+    """
+    Combines a list of buffer dumps for a particular radial/hpix 
+    cell into a single file maintaining peano ordering of particles
+    """
 
+    #Create unique file name to write combined file to
     for i, f in enumerate(flist):
         if 'mg' in f:
             fs = f.split('mg')
@@ -430,10 +492,14 @@ def combine_cell_list(flist):
         if i==(len(flist)-1):
             wf = f+'mg1'
 
+    #open the files
     hdrfmt = 'QIIfdQddd'
     rps = [open(f, 'rb') for f in flist]
     idxs = []
 
+    #read header, peano indices of all files
+    #combine into one header index by summing
+    #number of particles
     for i, rp in enumerate(rps):
         hi, idxi = read_radial_bin(rp)
         if i==0:
@@ -449,11 +515,12 @@ def combine_cell_list(flist):
         nadd = 0
 
     assert(np.sum(idx) == h[0])
+    #write new header, index
     with open(wf, 'wb') as wp:
         wp.write(struct.pack(hdrfmt, *h))
         wp.write(idx.tobytes())
 
-    #write particles
+    #put positions in buffer
     buff = Buffer(wf, dtype='f4')
     fmt = np.dtype(np.float32)
     for i in range(len(idx)):
@@ -464,7 +531,7 @@ def combine_cell_list(flist):
                 nadd += len(d)//3
 
     buff.write()
-
+    #put velocities in buffer
     for i in range(len(idx)):
         for j, idxi in enumerate(idxs):
             if idxi[i]:
@@ -473,11 +540,10 @@ def combine_cell_list(flist):
                 nadd += len(d)//3
                 
     buff.write()
-    print('Nwritten: {0}'.format(buff.nwritten//6))
-    print('Nparts: {0}'.format(h[0]))
+    #make sure buffer wrote everything we put in it
     assert(buff.nwritten//6 == h[0])
 
-    #write ids
+    #create new buffer for ids
     buff = Buffer(wf, dtype='u8')
     fmt = np.dtype(np.uint64)
     for i in range(len(idx)):
@@ -495,6 +561,17 @@ def combine_cell_list(flist):
 
 
 def write_empty_header(basepath, rbin, pix, header):
+    """
+    Write empty header containing the file and index nside.
+    Only want to do this for pixel 0 of each radial bin if
+    empty for addgals to reference.
+
+    basepath -- Path to write file to
+    rbin -- Index of radial bin
+    pix -- Healpix cell value
+    header -- The header containing the correct file nside, index
+              nside, cosmology and other metadata
+    """
     hdrfmt = 'QIIfdQddd'
     header[0] = 0
     with open('{0}_{1}_{2}'.format(basepath, rbin, pix), 'w') as fp:
@@ -502,7 +579,17 @@ def write_empty_header(basepath, rbin, pix, header):
 
 
 def process_cell(basepath, rbin, pix, rank=None, ncomb=10, header=None):
-
+    """
+    Collect combine all buffer dumps for a particular radial/hpix cell into
+    a single file.
+    
+    basepath -- Path to write the files to
+    rbin -- Index of the radial bin
+    pix -- Healpix cell value
+    rank -- The rank of the mpi process assigned to this cell
+    ncomb -- Number of buffers to combine at once
+    header -- Header containing lightcone metadata for this cell
+    """
     files = deque(glob('{0}_{1}_{2}_*'.format(basepath, rbin, pix)))
     if len(files)==0:
         if (rbin==0) & (pix==0):
@@ -510,9 +597,9 @@ def process_cell(basepath, rbin, pix, rank=None, ncomb=10, header=None):
         return
 
     tprint("    {2} Processing cell {0} {1}, nfiles = {3}".format(rbin, pix, rank, len(files)))
-    
     processed = []
 
+    #iterating through files combining them ncomb at a time
     while len(files)>1:
         npop = min(ncomb, len(files))
         flist = [files.popleft() for i in range(npop)]
@@ -528,7 +615,12 @@ def process_cell(basepath, rbin, pix, rank=None, ncomb=10, header=None):
 
 
 def read_radial_bin(filename, read_pos=False, read_vel=False, read_ids=False):
-
+    """
+    Read in a radial/hpix cell
+    
+    filename -- The name of the file to read
+    read_xxx -- Whether or not to read xxx
+    """
     hdrfmt = 'QIIfdQddd'
     idxfmt = np.dtype('i8')
     to_read = [read_pos, read_vel, read_ids]
@@ -566,32 +658,51 @@ def read_radial_bin(filename, read_pos=False, read_vel=False, read_ids=False):
 
 
 def nest2peano(pix, order):
-  subpix = np.array([ [ 0, 1, 3, 2 ], [ 3, 0, 2, 1 ], [ 2, 3, 1, 0 ], [ 1, 2, 0, 3 ],\
-                      [ 0, 3, 1, 2 ], [ 1, 0, 2, 3 ], [ 2, 1, 3, 0 ], [ 3, 2, 0, 1 ] ])
-  subpath = np.array([ [ 4, 0, 6, 0 ], [ 7, 5, 1, 1 ], [ 2, 4, 2, 6 ], [ 3, 3, 7, 5 ],\
-                       [ 0, 2, 4, 4 ], [ 5, 1, 5, 3 ], [ 6, 6, 0, 2 ], [ 1, 7, 3, 7 ] ])
-  face2path = np.array([ 2, 5, 2, 5, 3, 6, 3, 6, 2, 3, 2, 3 ])
-  face2peanoface = np.array([ 0, 5, 6, 11, 10, 1, 4, 7, 2, 3, 8, 9 ])
+    """
+    Convert nest ordered healpix cell values to peano indices
+    
+    pix -- Healpix cell value
+    order -- The order of the nside of pix
+    """
+    subpix = np.array([ [ 0, 1, 3, 2 ], [ 3, 0, 2, 1 ], [ 2, 3, 1, 0 ], [ 1, 2, 0, 3 ],\
+                            [ 0, 3, 1, 2 ], [ 1, 0, 2, 3 ], [ 2, 1, 3, 0 ], [ 3, 2, 0, 1 ] ])
+    subpath = np.array([ [ 4, 0, 6, 0 ], [ 7, 5, 1, 1 ], [ 2, 4, 2, 6 ], [ 3, 3, 7, 5 ],\
+                             [ 0, 2, 4, 4 ], [ 5, 1, 5, 3 ], [ 6, 6, 0, 2 ], [ 1, 7, 3, 7 ] ])
+    face2path = np.array([ 2, 5, 2, 5, 3, 6, 3, 6, 2, 3, 2, 3 ])
+    face2peanoface = np.array([ 0, 5, 6, 11, 10, 1, 4, 7, 2, 3, 8, 9 ])
+    
+    npix_ = 12*(1 << (2*order))
+    assert((pix >= 0).all() and (pix < npix_).all())
   
-  npix_ = 12*(1 << (2*order))
-  assert((pix >= 0).all() and (pix < npix_).all())
-  
-  face = pix>>(2*order)
-  path = face2path[face]
-  result = np.zeros(len(pix), dtype=np.int64);
-  shifts = np.arange(0, 2*order-1, 2)
-
-  for shift in shifts[::-1]:
-      spix = (pix>>shift) & 0x3
-      result <<= 2
-      result |= subpix[path,spix]
-      path = subpath[path,spix]
-
-  return result + ((face2peanoface[face])<<(2*order));
+    face = pix>>(2*order)
+    path = face2path[face]
+    result = np.zeros(len(pix), dtype=np.int64);
+    shifts = np.arange(0, 2*order-1, 2)
+    
+    for shift in shifts[::-1]:
+        spix = (pix>>shift) & 0x3
+        result <<= 2
+        result |= subpix[path,spix]
+        path = subpath[path,spix]
+        
+    return result + ((face2peanoface[face])<<(2*order));
 
 def map_LC_to_cells(namefile, outpath, simlabel, rmin, rmax, lfilenside, rr0,
-                    hfilenside=None, boxsize=1050.0, pmass=3.16):
-
+                    hfilenside=None):
+    """
+    Given a list of lightcone outputs from L-Gadget2, write the particles to radial/hpix cells 
+    breaking up higher radial bins into more refined healpix cells.
+    
+    namefile -- Name of the file containing the list of L-Gadget2 snapshots
+    outpath -- Path to write the reformatted files to
+    simlabel -- Label of the snapshots, i.e. lightcone_Snapshot
+    rmin -- Minimum radius to keep particles
+    rmax -- Maxmimum radius to keep particles
+    lfilenside -- Nside to write files to for rmin
+    rr0 -- First radius to refine nside at
+    hfilenside -- The highest nside to use
+    """
+    
     comm = MPI.COMM_WORLD
     size = comm.Get_size()
     rank = comm.Get_rank()
@@ -603,20 +714,35 @@ def map_LC_to_cells(namefile, outpath, simlabel, rmin, rmax, lfilenside, rr0,
     chunks = [blockpaths[i*step:(i+1)*step] for i in range(size)]
 
     header = write_to_cells_buff(chunks[rank], outbase, lfilenside=lfilenside,
-                                 hfilenside=hfilenside, rr0=rr0, boxsize=boxsize, pmass=pmass,
-                                 rmin=rmin, rmax=rmax)
+                                 hfilenside=hfilenside, rr0=rr0, rmin=rmin, rmax=rmax)
     return header
 
 def process_all_cells(outbase, rmin, rmax, rstep=25.0, rr0=300.0, lfilenside=1,
                       hfilenside=None, header=None):
+    """
+    Combine the buffer dumps from map_LC_to_cells into single files for 
+    each cell
+    
+    outbase -- Path to write files to
+    rmin -- Minimum radius used
+    rmax -- Maximum radius used
+    rstep -- Radial bin size
+    rr0 -- First radius to refine at 
+    lfilenside -- Nside to use for rmin
+    hfilenside -- Largest nside to use
+    header -- Default header
+    """
     
     comm = MPI.COMM_WORLD
     size = comm.Get_size()
     rank = comm.Get_rank()
-
+    
+    #determine the nside to use for each radial bin
     rads, rr, rnside, nr = create_refinement_plan(rmin, rmax, rstep, rr0,
                                                    lfilenside, hfilenside=hfilenside)
     rbins = np.arange((rmax-rmin)//rstep, dtype=np.int64)
+
+    #determine number of pixels for each radial bin
     rnpix = 12*rnside**2
     idx = np.cumsum(rnpix)
     idx = np.hstack([np.zeros(1),idx])
@@ -651,7 +777,7 @@ if __name__=='__main__':
     outbase = '{0}/{1}'.format(outpath,prefix)
     
     header = map_LC_to_cells(namefile, outpath, prefix, rmin, rmax, lfilenside,
-                             rr0, hfilenside=4, boxsize=1050.0, pmass=3.16)
+                             rr0, hfilenside=4)
 
     comm.Barrier()
 
